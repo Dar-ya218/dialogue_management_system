d33b9ca5b41fd6147663d4436340defd7c397bce
Dasha <dasharoura13465@gmail.com>
2024-12-19 10:39:34 +0100

    clean code

--- src/download/downloader.py
+++ src/download/downloader.py
@@ -2,43 +2,24 @@ from datasets import load_dataset
 import os
 import shutil
 
+DATASET_NAME = "josearangos/spanish-calls-corpus-Friends"
+SAVE_DIRECTORY = "data/audio"
+
 def descargar_dataset(dataset_name, save_directory):
-    """
-    Descarga un dataset completo desde Hugging Face y guarda los archivos localmente.
-    """
-    # Cargar el dataset
     print(f"Cargando dataset '{dataset_name}' desde Hugging Face...")
     dataset = load_dataset(dataset_name)
 
-    # Mostrar los splits disponibles
-    print("Splits disponibles en el dataset:")
-    print(dataset)
-
-    # Crear directorio de destino si no existe
     os.makedirs(save_directory, exist_ok=True)
 
-    # Iterar sobre cada fila y guardar los archivos de audio
-    for index, row in enumerate(dataset["train"]):  # Cambia "train" si el split es diferente
+    for index, row in enumerate(dataset["train"]):
         if "audio" in row:
-            audio_path = row["audio"]["path"]  # Ruta al archivo de audio local
+            audio_path = row["audio"]["path"] 
             destination_path = os.path.join(save_directory, f"audio_{index}.mp3")
             
-            # Copiar el archivo al directorio de destino
             shutil.copy(audio_path, destination_path)
             print(f"Archivo guardado: {destination_path}")
         else:
             print(f"No se encontró audio en la fila {index + 1}.")
 
 if __name__ == "__main__":
-    # Nombre del dataset en Hugging Face
-    DATASET_NAME = "josearangos/spanish-calls-corpus-Friends"
-    
-    # Directorio donde guardar los archivos
-    SAVE_DIRECTORY = "data/audio"
-
-     # Crear la carpeta si no existe
-    os.makedirs(SAVE_DIRECTORY, exist_ok=True)
-    print(f"Guardando archivos en: {os.path.abspath(SAVE_DIRECTORY)}")
-    
-    # Descargar el dataset
     descargar_dataset(DATASET_NAME, SAVE_DIRECTORY)
